37cc429734fc9c090ccae673f85f3d5ce97aa8b7
Jacob Cathcart <jaccathc@uat.edu>
2019-10-05 14:30:47 -0700

    Milestone 2

---
+++ Assets/Scripts/enemyMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyMovement : MonoBehaviour
+{
+    private Transform tf; // Create a variable for our transform component
+    public float tSpeed; // Create a variable for the degrees we rotate in one frame draw
+    public float mSpeed; // Create a variable for the lateral speed
+
+    void Start()
+    {
+        tf = GetComponent<Transform>(); // Load our transform component into our variable
+    }
+    void Update()
+    {
+        tf.Rotate(0.0f, 0.0f, tSpeed);
+        tf.Translate(mSpeed, 0.0f, 0.0f);
+    }
+}
---
+++ Assets/Scripts/gameManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameManager : MonoBehaviour
+{
+    public static gameManager instance;
+    public int score = 0;
+    public int lives = 3;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+            Debug.Log("Warning: A second game manager was detected and destroyed.");
+        }
+    }
+}
